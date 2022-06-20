6eeb5e00f1de1dd610513032e9ba8f9fd8ec94d7
Timothy Tomas <timothyjtee@gmail.com>
2022-06-20 15:41:34 -0700

    add html elements

--- main.js
+++ main.js
@@ -2,18 +2,19 @@ document.getElementById('getButton').addEventListener('click', apiRequest)
 
 async function apiRequest(){
     const festivalName = document.getElementById('name').value.toLowerCase()
+    
     try{
         const response = await fetch(`https://music-festival-info.herokuapp.com/api/${festivalName}`)
         const data = await response.json()
 
         console.log(data)
-        document.getElementById('name').innerText = data.speciesName
-        document.getElementById('date').innerText = data.homeworld
-        document.getElementById('genre').innerText = data.features
-        document.getElementById('description').innerText = data.interestingFact
-        document.getElementById('attendance').innerText = data.notableExamples
-        document.getElementById('website').src = data.image
-        document.getElementById('image').innerText = data.speciesName
+        document.getElementById('festival').innerText = festivalName;
+        document.getElementById('date').innerText = data.date;
+        document.getElementById('genre').innerText = data.genre
+        document.getElementById('description').innerText = data.desscription;
+        document.getElementById('attendance').innerText = data.attendance;
+        document.getElementById('website').innerText = data.website;
+        document.getElementById('image').src = data.image;
     }catch(error){
         console.log(error)
     }
